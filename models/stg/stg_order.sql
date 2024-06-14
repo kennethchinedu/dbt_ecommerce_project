87ac12023f698f0fcad6711e2a22e32375e7b534
kennethchinedu <anamsken60@gmail.com>
2024-06-14 13:42:11 +0100

    added test to staging model

--- models/stg/stg_order.sql
+++ models/stg/stg_order.sql
@@ -7,6 +7,7 @@ WITH
             o_totalprice AS total_price,
             o_orderdate AS order_date,
             {{extract_month('o_orderdate')}} AS order_month,
+            {{ dbt_date.day_name("o_orderdate", short=false) }} as order_day,
             o_orderpriority AS order_priority,
             o_clerk AS clerk,
             o_shippriority AS shippriority, 
